85972bd82559612603878be709689c041ebd3bbc
aliho <a.hodavand@yahoo.com>
2021-03-19 23:11:47 +0330

    remove all caches with blog-post tag after seeding database

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Cache;
+
 use App\Models\User;
 use App\Models\BlogPost;
 use App\Models\Comment;
@@ -16,6 +18,13 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        if ($this->command->confirm('Want to refresh database?', true)) {
+            $this->command->call('migrate:refresh');
+            $this->command->info('Database is seeded');
+        }
+
+        Cache::tags(['blog-post'])->flush();
+
         $this->call([UserSeeder::class, BlogPostSeeder::class, CommentSeeder::class]);
 
     }
