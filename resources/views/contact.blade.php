9c11771d0710509bb926b81622c5a4dd3a358843
aliho <a.hodavand@yahoo.com>
2021-03-22 11:09:56 +0430

    formatting

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -1,21 +1,21 @@
 @extends('layouts.app')
 
 @section('title')
-    Contact
+Contact
 @endsection
 
 @section('content')
 <div class="container">
-        <div class="jumbotron">
-            <h1 class="display-3">Contact</h1>
-            <p class="lead">Here is our contact page!</p>
-            @can('home.contact')
-                <hr class="my-2">
-                <p>More info</p>
-                <p class="lead">
-                    <a class="btn btn-primary btn-lg" href="Jumbo action link" role="button">Some Action</a>
-                </p>
-            @endcan
-        </div>
+    <div class="jumbotron">
+        <h1 class="display-3">Contact</h1>
+        <p class="lead">Here is our contact page!</p>
+        @can('home.contact')
+            <hr class="my-2">
+            <p>More info</p>
+            <p class="lead">
+                <a class="btn btn-primary btn-lg" href="Jumbo action link" role="button">Some Action</a>
+            </p>
+        @endcan
     </div>
+</div>
 @endsection
