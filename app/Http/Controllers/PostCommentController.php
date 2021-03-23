7bba5a5e80dbb424732de2bd1d27ee325a2c5b06
aliho <a.hodavand@yahoo.com>
2021-03-23 12:38:07 +0430

    sending email with markdown

--- app/Http/Controllers/PostCommentController.php
+++ app/Http/Controllers/PostCommentController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\StoreComment;
 use App\Mail\CommentPosted;
+use App\Mail\CommentPostedMarkdown;
 use App\Models\BlogPost;
 use Illuminate\Support\Facades\Mail;
 
@@ -21,7 +22,7 @@ class PostCommentController extends Controller
             'user_id' => $request->user()->id,
         ]);
 
-        Mail::to($post->user)->send(new CommentPosted($comment));
+        Mail::to($post->user)->send(new CommentPostedMarkdown($comment));
 
         return redirect()->back()->withStatus('Comment added successfully!');
     }
